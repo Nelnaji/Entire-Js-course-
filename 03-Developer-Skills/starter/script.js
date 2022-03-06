9c873aa0ef6d7b9a3af170647f909b55e7d166ec
unknown <nawfal.phoenix@gmail.com>
2022-03-06 14:06:45 +0100

    first commit

---
+++ 03-Developer-Skills/starter/script.js
@@ -0,0 +1,13 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+const arr = [17, 21, 23];
+const arr2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]}°C in ${i + 1} days `);
+  }
+};
+printForecast(arr);
+printForecast(arr2);
