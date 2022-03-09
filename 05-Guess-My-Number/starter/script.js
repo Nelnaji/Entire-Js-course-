326e3e5a553fc6f6dd5c326700615070ad38cdf4
unknown <nawfal.phoenix@gmail.com>
2022-03-09 13:23:22 +0100

    some changes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+let score = 20;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Not a number';
+  } else if (guess === secretNumber) {
+    document.querySelector(
+      '.message'
+    ).textContent = `Good job the Number was indeed ${secretNumber}`;
+  } else if (guess > secretNumber && score > 1) {
+    document.querySelector('.message').textContent = "you're too high";
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber && score > 1) {
+    document.querySelector('.message').textContent = "you're too low";
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent = 'you lost the game';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
