dfd7f9ac19d8c7d74b0370a05792d1d87115d4c8
nawfal elkhaznagi <nawfal.phoenix@gmail.com>
2023-01-20 21:46:51 +0100

    .

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -275,3 +275,21 @@ const seven = menu[0] ?? "test";
 */
 
 // Sets
+
+// coding challenge 3
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = [];
