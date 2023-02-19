d4d9eff52327f2fa1a85e2c7012e8971d97f5c80
nawfal elkhaznagi <nawfal.elkhaznagi@gmail.com>
2023-02-19 01:46:55 +0100

    working on constructor function

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // you should never have a method inside a constructor function
+  // this.calcAge = function () {
+  //   console.log(2023 - this.birthYear);
+  // };
+};
+
+const nawfal = new Person('Nawfal', 1993);
+
+console.log(nawfal.firstName);
