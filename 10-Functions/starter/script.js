e3c30290e2032a052c332665d466f0613940f6ff
naofel <nawfal.elkhaznagi@gmail.com>
2024-05-30 22:22:36 +0200

    completed function section

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -308,25 +308,37 @@
 // 
 // Teacher's answer
 
-const pollBtn = document.querySelector('.poll');
-const poll = {
+// const pollBtn = document.querySelector('.poll');
+// const poll = {
 
-    question: "What is your favourite programming language?",
-    options: ['0:JavaScript', '1:Python', '2:Rust', '3: C++'],
-    answers: new Array(4).fill(0),
-    registerNewAnswer() {
-       const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
-       console.log(answer)
+//     question: "What is your favourite programming language?",
+//     options: ['0:JavaScript', '1:Python', '2:Rust', '3: C++'],
+//     answers: new Array(4).fill(0),
+//     registerNewAnswer() {
+//        const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
+//        console.log(answer)
         
-        typeof answer === 'number' && answer < this.answers.length
-
+//         typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++
+//         this.displayResults();
+//         this.displayResults('string');
     
-    }
-    
-}
+//     },
+
+//     displayResults(type = 'array') {
+//          if (type === 'array'){
+//             console.log(this.answers);
+//          } else if (type === 'string') {
+//             console.log(`Poll resuslts are ${this.answers.join(', ')}`)
+//          }
+//     }
+// }
 
 
-pollBtn.addEventListener('click', poll.registerNewAnswer.bind(poll))
+// pollBtn.addEventListener('click', poll.registerNewAnswer.bind(poll))
+
+
+
+// poll.displayResults.call({answers: [5,2,3]}, 'string')
 
 // 
 // End of coding challenge
@@ -416,3 +428,18 @@ pollBtn.addEventListener('click', poll.registerNewAnswer.bind(poll))
 // const perGroup = 500;
 
 // boardPassengers(261, 15)
+
+
+(function () {
+
+    const header = document.querySelector('h1');
+    header.style.color= 'red';
+
+    document.querySelector('body').addEventListener('click', function () {
+        header.style.color='blue'; 
+
+    }
+)
+        
+
+})();
