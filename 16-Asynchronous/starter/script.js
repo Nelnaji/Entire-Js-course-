fba8433e43bd0c30ed67d78dd77d00b5de7ff50a
naofel <nawfal.elkhaznagi@gmail.com>
2024-06-06 22:30:32 +0200

    combinator functions

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -8,6 +8,7 @@ const countriesContainer = document.querySelector('.countries');
 const renderError = msg => {
 
   countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
   
   }
   
@@ -36,13 +37,29 @@ countriesContainer.insertAdjacentHTML('beforeend', countryHTML);
 }
 
 
+
+const getJSON = function (url,errorMsg = 'Something is wrong') {
+
+
+  return fetch(url).then(
+    
+  response => {
+
+    if(!response.ok){
+      throw new Error(`${errorMsg} (${response.status})`)
+    }
+    
+    return response.json()}
+
+)};
+
 ///////////////////////////////////////
 
-
+/*
 // not using the v3.1 because it's too slow
 // request.open('GET', 'https://restcountries.com/v3.1/name/morocco');
 
-/*
+
 
 const getCountryAndNeighbour = function(country) {
   // AjAX call country 1
@@ -92,9 +109,10 @@ getCountryAndNeighbour('morocco');
 
 // when we want multiple function to run asynchronuously in sequence, we call it callback Hell
 
-*/
+
 
 // new way of making api calls
+
 
 const getCountryData = function(country) {
 
@@ -132,103 +150,88 @@ const getCountryData = function(country) {
 
 
 
-// const getJSON = function (url,errorMsg = 'Something is wrong') {
-
-
-//   return fetch(url).then(
-    
-//   response => {
-
-//     if(!response.ok){
-//       throw new Error(`${errorMsg} (${response.status})`)
-//     }
-    
-//     return response.json()}
-
-// )};
-
 
 // simply used arrow functions
-// const getCountryData = function(country) {
+const getCountryData = function(country) {
 
-  // country 1
-// getJSON(`https://countries-api-836d.onrender.com/countries/name/${country}`, 'Country not found').then( ([data]) =>  {
+  country 1
+getJSON(`https://countries-api-836d.onrender.com/countries/name/${country}`, 'Country not found').then( ([data]) =>  {
 
-//     renderCountry(data)
-//   const neighbour = data.borders?.[0]
-//   // country 2 
-//   return getJSON(`https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`, 'Neighbour not found')
+    renderCountry(data)
+  const neighbour = data.borders?.[0]
+  // country 2 
+  return getJSON(`https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`, 'Neighbour not found')
     
-// }).then(data => renderCountry(data, "neighbour")).catch( err => console.error(renderError(`Something went wrong 💥💥💥💥 ${err.message} Try again`))).finally(()=>{
+}).then(data => renderCountry(data, "neighbour")).catch( err => console.error(renderError(`Something went wrong 💥💥💥💥 ${err.message} Try again`))).finally(()=>{
 
-//     countriesContainer.style.opacity = 1;
+    countriesContainer.style.opacity = 1;
 
-//   });
-
-
+  });
 
 
 
 
-// };
+
+
+};
 
 // la method then s'execute quand la promesse est fulfilled, la methode catch s'execute quand la promesse fail. alors que finally s'execute peu importe si la promesse est remplie ou pas
 
-/*
+
 // test dataCoordinates 1: 52.508, 13.381 (Latitude, Longitude)
-§ Coordinates 2: 19.037, 72.873
-§ Coordinates 3: -33.933, 18.474
-*/
-// btn.addEventListener('click', function(){
+// § Coordinates 2: 19.037, 72.873
+// § Coordinates 3: -33.933, 18.474
 
-//   whereAmI(-33.933, 18.474)
+btn.addEventListener('click', function(){
 
-// })
+  whereAmI(-33.933, 18.474)
 
-
-// const whereAmI = (lat,lng) =>{
+})
 
 
-//   fetch(
-//     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
-//   ).then(response => {
+const whereAmI = (lat,lng) =>{
+
+
+  fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+  ).then(response => {
   
   
-//     return response.json()
+    return response.json()
    
-//   }).then(data => {
+  }).then(data => {
 
-//     getCountryData(data.countryName)
+    getCountryData(data.countryName)
      
-//   }).catch( err => console.error(renderError((`Something went wrong 💥💥💥💥 ${err.message} Try again`))).finally(()=>{
+  }).catch( err => console.error(renderError((`Something went wrong 💥💥💥💥 ${err.message} Try again`))).finally(()=>{
 
-//     countriesContainer.style.opacity = 1;
+    countriesContainer.style.opacity = 1;
 
-//   }));
+  }));
 
-// }
-
-
+}
 
 
-// console.log(`You are in ${data.city}, ${data.countryName}`);
 
-// console.log('test start');
 
-// setTimeout(() => console.log('o sec timer'),0 );
+console.log(`You are in ${data.city}, ${data.countryName}`);
 
-// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+console.log('test start');
 
-// Promise.resolve('Resolved promise 2').then( res=> {
+setTimeout(() => console.log('o sec timer'),0 );
+
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then( res=> {
   
-//   for ( let i = 0 ; i<1000000000; i++){};
-//   console.log(res)
+  for ( let i = 0 ; i<1000000000; i++){};
+  console.log(res)
 
-// } );
+} );
 
 
 // console.log('test end');
-/*
+
 const lotteryPromise = new Promise(function(resolve,reject) {
 
 console.log('lottery draw is happening')
@@ -247,9 +250,9 @@ console.log('lottery draw is happening')
 
 lotteryPromise.then( res => console.log(res)
   ).catch(err => console.error(err))
-*/
+
 // promisifying etTimeout
-/*
+
   const wait = function (seconds) {
 
 return new Promise ( function (resolve) {
@@ -271,15 +274,13 @@ return new Promise ( function (resolve) {
   console.log('waited 3 sec')
   return wait(1)
 }).then(() => console.log('4s'))
-*/
+
 
 const getPosition = function () {
 
 return new Promise(function (resolve, reject) {
 
-
-
-  navigator.geolocation.getCurrentPosition(resolve,reject);
+navigator.geolocation.getCurrentPosition(resolve,reject);
 });
 
 };
@@ -324,15 +325,83 @@ getPosition().then(pos => {
   btn.addEventListener('click', whereAmI)
 
 
-  let numbers = [1,3,6,1]
-  let squaredNumbers = []
 
-  function squareSum(numbers)
-  {
 
-console.log(numbers.reduce((a,b) => a+a , 10))
 
-   }
+const whereAmI = async function(country){
 
-squareSum(numbers)
-  
+try{
+  const pos = await getPosition();
+
+  const{  latitude:lat, longitude:lng} = pos.coords
+
+  const resGeo = await fetch(
+  `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+);
+
+
+if(!resGeo.ok) {
+  throw new Error('problem getting location data')
+}
+const dataGeo = await resGeo.json();
+
+console.log(dataGeo)
+
+// fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`).then( res => console.log(res))
+
+
+
+
+const res = await fetch(`https://countries-api-836d.onrender.com/countries/name/${dataGeo.countryName}`)
+
+if (!res.ok) {
+  throw new Error('problem getting country')
+}
+const data = await res.json();
+
+renderCountry(data[0])
+
+
+countriesContainer.style.opacity = 1;
+
+return `I live in ${dataGeo.countryName}`
+}catch(err){
+  console.error(err.message);
+
+}
+
+ };
+
+//  try{
+//   let y = 1;
+//   const x = 3;
+
+//   pageYOffset = 3;
+//  } catch(err) {
+
+//   alert(err.message)
+//  }
+
+
+*/
+
+
+// always try catch block when dealing with async function
+
+const get3Countries = async function(c1,c2,c3) {
+try{
+// const [data1] = await getJSON(`https://countries-api-836d.onrender.com/countries/name/${c1}`);
+// const [data2] = await getJSON(`https://countries-api-836d.onrender.com/countries/name/${c2}`);
+// const [data3] = await getJSON(`https://countries-api-836d.onrender.com/countries/name/${c3}`);
+
+let data = await Promise.all([getJSON(`https://countries-api-836d.onrender.com/countries/name/${c1}`),getJSON(`https://countries-api-836d.onrender.com/countries/name/${c2}`),getJSON(`https://countries-api-836d.onrender.com/countries/name/${c3}`)])
+
+console.log(data.map(d => d[0].capital))
+
+
+
+}catch(error){
+console.log(error.message)
+}}
+
+get3Countries('germany', 'france', 'belgium')
